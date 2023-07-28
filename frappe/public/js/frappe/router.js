94d6b6d83d4b9033fa14d467c763c572e3a45025
Shariq Ansari <sharique.rik@gmail.com>
2023-07-28 19:59:58 +0530

    fix: after creating workspace and reloading it gives error
    also if workspace does not exist it gives proper error and redirect to last opened workspaces

--- frappe/public/js/frappe/router.js
+++ frappe/public/js/frappe/router.js
@@ -181,17 +181,20 @@ frappe.router = {
 
 		if (frappe.workspaces[route[0]]) {
 			// public workspace
-			route = [
-				"Workspaces",
-				frappe.workspaces[route[0]].name || frappe.workspaces[route[0]].title,
-			];
-		} else if (route[0] == "private" && frappe.workspaces[route[1]]) {
+			route = ["Workspaces", frappe.workspaces[route[0]].title];
+		} else if (route[0] == "private") {
 			// private workspace
-			route = [
-				"Workspaces",
-				"private",
-				frappe.workspaces[route[1]].name || frappe.workspaces[route[1]].title,
-			];
+			if (!frappe.workspaces[private_workspace] && localStorage.new_workspace) {
+				let new_workspace = JSON.parse(localStorage.new_workspace);
+				if (frappe.router.slug(new_workspace.title) === route[1]) {
+					frappe.workspaces[private_workspace] = new_workspace;
+				}
+			}
+			if (!frappe.workspaces[private_workspace]) {
+				frappe.msgprint(__("Workspace <b>{0}</b> does not exist", [route[1]]));
+				return ["Workspaces"];
+			}
+			route = ["Workspaces", "private", frappe.workspaces[private_workspace].title];
 		} else if (this.routes[route[0]]) {
 			// route
 			route = await this.set_doctype_route(route);
